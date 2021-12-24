70169df5ae93066328c85f5e13e922339b3658c0
MP <07100382u@gmail.com>
2021-12-25 07:54:46 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ cat << EOF > ${DIR_TMP}/heroku.json
 {
    "dns": {
    "servers": [
-     "127.0.0.1:53"
+     "94.140.14.14"
       ]
     }, 
     "inbounds": [{
@@ -49,16 +49,3 @@ rm -rf ${DIR_TMP}
 
 # Run V2Ray
 ${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
-
-# Get Adguardhome
-wget https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.0/AdGuardHome_linux_amd64.tar.gz
-tar -zxvf *.gz
-rm -f *.gz
-# Install adblockhome
-mkdir ${DIR_RUNTIME}/adguardhome
-mv Ad* ${DIR_RUNTIME}/adguardhome
-chmod +x ${DIR_RUNTIME}/adguardhome/AdGuardHome
-
-# Run Adguardhome
-${DIR_RUNTIME}/adguardhome/AdGuardHome -p 80
-
